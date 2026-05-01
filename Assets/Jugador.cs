a7529d73eea89299e965727e6de446a3ac9452ff
trinodhz32 <trinodhz32@gmail.com>
2026-05-01 15:00:36 -0600

    moviendo al jugador

--- Assets/Jugador.cs
+++ Assets/Jugador.cs
@@ -1,16 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class Jugador : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] public int limiteX = 23;
+    [SerializeField] public float VelocidadPaddle = 5f;
+    private Camera mainCam;
+
     void Start()
     {
-        
+        mainCam = Camera.main;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        Vector2 mouseScreenPos = Mouse.current.position.ReadValue();
+        Vector3 mouseWorldPos = mainCam.ScreenToWorldPoint(new Vector3(mouseScreenPos.x, mouseScreenPos.y, mainCam.transform.position.y));
+          Vector3 pos = this.transform.position;
+
+        // if (Input.GetKey(GetCode.RightArrow))
+        // {
+        //     this.transform.Translate(Vector3.right * VelocidadPaddle * Time.deltaTime);
+        // }
+        // if (Input.GetKey(KeyCode.LeftArrow))
+        // {
+        //     this.transform.Translate(Vector3.left * VelocidadPaddle * Time.deltaTime);
+        // }
         
+      
+        pos.x = mouseWorldPos.x;
+        if (pos.x < -limiteX)
+        {
+            pos.x = -limiteX;
+        
+        }
+        else if (pos.x > limiteX)
+        {
+            pos.x = limiteX;
+        }
+        this.transform.position = pos;
     }
 }
