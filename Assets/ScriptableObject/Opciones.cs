ca2be960aff6d45b0ae1b0ba423fc1efd49a6803
trinodhz32 <trinodhz32@gmail.com>
2026-07-15 12:57:48 -0600

    Modulo 14 tarea

--- Assets/ScriptableObject/Opciones.cs
+++ Assets/ScriptableObject/Opciones.cs
@@ -6,13 +6,13 @@ using System.Collections.Generic;
 public class Opciones : PuntajePersistente
 {
 public float velocidadBola = 30;
-public dificultad NivelDificultad = dificultad.facil;
+public dificultad NivelDificultad = dificultad.Facil;
 
 public enum dificultad
     {
-        facil,
-        normal,
-        dificil
+        Facil,
+        Normal,
+        Dificil
     }
     public void CambiarVelocidad(float nuevaVelocidad)
     {
@@ -22,4 +22,5 @@ public enum dificultad
     {
         NivelDificultad = (dificultad)nuevaDificultad;
     }
+    
 }
